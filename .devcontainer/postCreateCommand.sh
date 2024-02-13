ef2d5b8e3288fbfe2f07e24db9afea1f5d938e2f
Tigit Labs <61022457+tigitlabs@users.noreply.github.com>
2024-02-14 00:20:20 +0100

    Feat devcontainer (#5)
    * Add .devcontainer files for Docker configuration
    
    This commit adds the necessary files for configuring Docker in the development container. It includes the Dockerfile, devcontainer.json, and a postCreateCommand script for setting up dotfiles.
    
    * Commented out setting hosts file in ubuntu.yml
    
    * removed unused plugin
    
    * added start-up command

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Setting up dotfiles..."
+mkdir -p /home/vscode/.dotfiles && \
+ln -s /workspaces/dotfiles/* /home/vscode/.dotfiles/ && \
+ln -s /workspaces/dotfiles/.git /home/vscode/.dotfiles
